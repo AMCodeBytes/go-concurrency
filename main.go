0a889f419bc7ba66558d46d80786d7621437f3e5
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-06-18 16:33:07 +0100

    Created fetch to weatherbit.io
    This is a working call to the weatherbit api. This part fetches the weather for 10 cities without Go Concurrency. Operation takes around 1 second.

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var apiKey string
+
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+	startNow := time.Now()
+
+	apiKey = getEnvVariable("API_KEY")
+	cities := []string{"London", "Glasgow", "Liverpool", "Perth", "New+York", "York", "Paris", "Tokyo", "Kyoto", "Seoul"}
+
+	for _, city := range cities {
+		data := fetchWeather(city)
+		fmt.Println("This is the data", data)
+	}
+
+	fmt.Println("This operation took:", time.Since(startNow))
+}
+
+func fetchWeather(city string) interface{} {
+	var WeatherData struct {
+		Data []struct {
+			Temp     float64 `json:"temp"`
+			CityName string  `json:"city_name"`
+		} `json:"data"`
+	}
+
+	url := fmt.Sprintf("https://api.weatherbit.io/v2.0/current?city=%s&key=%s", city, apiKey)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Printf("Error fetching weather for %s: %s\n", city, err)
+		return WeatherData
+	}
+
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&WeatherData); err != nil {
+		fmt.Printf("Error fetching weather data for %s: %s\n", city, err)
+		return WeatherData
+	}
+
+	return WeatherData
+}
